6d69b3fc67d28897a14b1eb4520eddfb6dec490c
nikhil <nikhil.tiwari@vayana.com>
2025-08-02 01:23:24 +0530

    added company marquee

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "@mantine/core/styles.css";
+import "@mantine/carousel/styles.css";
 
 function App() {
   const theme = createTheme({
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,15 @@
 import Header from "../header/Header";
 import Companies from "../landingPage/Companies";
 import DreamJob from "../landingPage/DreamJob";
+import JobCategory from "../landingPage/JobCategory";
 
 const HomePage = () => {
   return (
     <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
       <Header />
       <DreamJob />
-      <Companies /> 
+      <Companies />
+      <JobCategory />
     </div>
   );
 };
