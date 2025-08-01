2b165353eb8e3c5cda90dfd74b90ac3fffc5a5bc
nikhil <nikhil.tiwari@vayana.com>
2025-08-01 23:41:04 +0530

    added header basic setup

--- src/header/Navlink.tsx
+++ src/header/Navlink.tsx
@@ -12,7 +12,7 @@ const Navlink = () => {
   return (
     <div className="flex gap-5 text-mine-shaft-300 h-full items-center">
       {links.map((link, index) => (
-        <div className={`border-t-[3px] h-full`}>
+        <div className={`${location.pathname == link.url?"border-bright-sun-400 text-bright-sun-400":"border-transparent"} border-t-[3px] h-full flex items-center`}>
           <Link key={index} to={link.url}>
             {link.name}
           </Link>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
 import Header from "../header/Header";
+import Companies from "../landingPage/Companies";
+import DreamJob from "../landingPage/DreamJob";
 
 const HomePage = () => {
   return (
     <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
       <Header />
+      <DreamJob />
+      <Companies /> 
     </div>
   );
 };
